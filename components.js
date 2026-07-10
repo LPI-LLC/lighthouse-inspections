b6f217dce71d84713beb564a884b4d6437ef99ba
Lighthouse Property Inspections, LLC <andrew@lighthouse-inspections.com>
2026-07-10 15:18:46 -0400

    Update footer service links

--- components.js
+++ components.js
@@ -300,11 +300,14 @@ desc: 'Focused verification of listed repairs, contractor repair lists, lender-r
             '</div>' +
             '<div class="footer-links">' +
               '<h4>Services</h4>' +
-              '<a href="' + linkPrefix + '#services">Residential Inspections</a>' +
-              '<a href="' + linkPrefix + '#services">WDO Inspections</a>' +
-              '<a href="' + linkPrefix + '#services">Thermal Imaging</a>' +
-              '<a href="' + linkPrefix + '#services">Chimney, Deck &amp; Roof</a>' +
-              '<a href="' + linkPrefix + '#services">Plumbing, Electrical &amp; HVAC</a>' +
+                        <a href="' + linkPrefix + '#services">Residential Inspections</a>
+                        <a href="' + linkPrefix + '#services">WDO Inspections</a>
+                        <a href="' + linkPrefix + '#services">Thermal Imaging</a>
+                        <a href="' + linkPrefix + '#services">Radon Testing</a>
+                        <a href="' + linkPrefix + '#services">Repair Verification</a>
+                        <a href="' + linkPrefix + '#services">Return Inspections</a>
+                        <a href="' + linkPrefix + '#services">Chimney, Deck & Roof</a>
+                        <a href="' + linkPrefix + '#services">Plumbing, Electrical & HVAC</a>
             '</div>' +
             '<div class="footer-contact">' +
               '<h4>Contact</h4>' +
